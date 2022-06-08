9632caf99584dfb224ce4dbb1136a28616eb4963
Hamza Ghani <h.u.f.ghani@gmail.com>
2022-06-08 16:48:26 +0100

    :construction: wip: add in the basic layout of the index and other pages

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import Icon from 'svelte-awesome';
+	import githubSquare from 'svelte-awesome/icons/githubSquare';
+	import instagram from 'svelte-awesome/icons/instagram';
+	import linkedinSquare from 'svelte-awesome/icons/linkedinSquare';
+	import twitterSquare from 'svelte-awesome/icons/twitterSquare';
+		</script>
+
+<footer class="mt-4 text-lg text-center w-full">
+	<p class="text-sm" aria-label="© 2017 - ${new Date().getFullYear()}, Hamza U. F. Ghani">
+		© 2017 - {new Date().getFullYear()}, Hamza U. F. Ghani
+	</p>
+	<div class="items-center flex flex-row justify-center pt-4 pb-4">
+		<div class="mr-2">
+			<a
+				href="https://github.com/HUFGhani"
+				class="no-underline focus:outline-none"
+				aria-label="github"
+				target="_blank"
+			>
+				<Icon data={githubSquare} class="w-7 h-7" />
+			</a>
+		</div>
+		<div class="mr-2">
+			<a
+				href="https://www.linkedin.com/in/hamza-u-f-ghani/"
+				class="no-underline focus:outline-none"
+				aria-label="linkedin"
+				target="_blank"
+			>
+				<Icon data={linkedinSquare} class="w-7 h-7" />
+			</a>
+		</div>
+		<div class="mr-2">
+			<a
+				href="https://twitter.com/the_ghani"
+				class="no-underline focus:outline-none"
+				aria-label="twitter"
+				target="_blank"
+			>
+				<Icon data={twitterSquare} class="w-7 h-7" />
+			</a>
+		</div>
+		<div class="mr-2">
+			<a
+				href="https://www.instagram.com/the.ghani/"
+				class="no-underline focus:outline-none"
+				aria-label="linkedin"
+				target="_blank"
+			>
+				<Icon data={instagram} class="w-7 h-7" />
+			</a>
+		</div>
+	</div>
+
+	<p class="text-sm pb-4">Built with SvelteKit · Hosted on S3 and distributed by CloudFront</p>
+</footer>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+	import Footer from '../lib/components/Footer.svelte';
+	import NavBar from '../lib/components/NavBar.svelte';
+</script>
+
+<div
+	class="flex flex-col content-center justify-center items-center xl:max-w-6xl mt-0 mx-auto sm:px-2 md:px-2 lg:px-20 xl:px-20 pt-10"
+>
+	<NavBar />
+	<div class="sm:w-full md:w-11/12 lg:w-11/12 sm:p-2 md:p-2 lg:pt-40 xl:pt-40 text-center">
+		<slot />
+	</div>
+	<Footer />
+</div>
