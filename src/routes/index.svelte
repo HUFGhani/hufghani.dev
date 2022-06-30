29601255f41b69accb78ad05197e5f743c15a6e1
Hamza Ghani <h.u.f.ghani@gmail.com>
2022-06-30 17:33:19 +0100

    :lipstick: style: restyle the index to follow new desgin

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,20 +6,21 @@
 	pageTitle={'Hello World'}
 	pageDescription={"My own little space on the internet, where I write about what I've learned from my day job."}
 />
-<section class="pb-8">
-	<div
-		class="flex flex-col-reverse md:flex-col lg:flex-row xl:flex-row  justify-between w-full px-5"
-	>
-		<div
-			class="flex flex-col md:text-center lg:text-left xl:text-left mg:justify-center lg:justify-start xl:justify-start items-center md:items-center lg:items-start "
-		>
-			<h1 class="text-5xl font-bold">Hi, I'm Hamza.</h1>
-			<p class="pt-4 break-words w-5/6">
-				A Father, Husband, Brother, Son and Software Engineer from Manchester, United Kingdom.
-			</p>
+<div class="flex flex-col md:flex-col lg:flex-row xl:flex-row justify-evenly w-full px-5">
+	<div class="flex flex-col text-left justify-start items-start ">
+		<h1 class="text-8xl font-bold">Hi, I'm Hamza.</h1>
+		<p class="pt-4 break-words lg:w-10/12 text-xl pb-5 ">
+			A Father, Husband, Brother, Son and Software Engineer from Manchester, United Kingdom.
+		</p>
+	</div>
+	<div class="flex flex-col break-words lg:w-6/12 gap-10 text-lg prose">
+		<div>
+			<h1 class="underline underline-offset-8 text-xl">Professionally:</h1>
+			<p>s</p>
 		</div>
-		<div class="pb-8">
-			
+		<div>
+			<h1 class="underline underline-offset-8 text-xl">Personally:</h1>
+			<p>a</p>
 		</div>
 	</div>
-</section>
+</div>
