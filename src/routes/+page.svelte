65067c36fc641f20bc1d5b1c5cbb744d35dd4185
Hamza Ghani <h.u.f.ghani@gmail.com>
2023-07-12 07:23:38 +0100

    :green_heart: ci(custom): update build script

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,8 +18,8 @@
 	<div class="flex flex-col md:flex-col lg:flex-row xl:flex-row justify-evenly w-full px-5">
 		<div class="flex flex-col text-left justify-start items-start lg:pr-52">
 			<h1 class="text-8xl font-bold">Hi, I'm Hamza;</h1>
-			<p class="pt-4 break-words lg:w-10/12 text-xl pb-5 ">
-				A Father Husband, Brother, Son and Software Engineer from Manchester, United Kingdom.
+			<p class="pt-4 break-words lg:w-10/12 text-xl pb-5">
+				A Father, Husband, Brother, Son and Software Engineer from Manchester, United Kingdom.
 			</p>
 		</div>
 		<div class="flex flex-col break-words lg:w-6/12 gap-10 text-lg prose">
