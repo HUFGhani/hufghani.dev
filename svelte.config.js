5fec17dca899a8d2764ff2dc9e4b981bc16619d2
Hamza Ghani <h.u.f.ghani@gmail.com>
2024-11-21 17:04:28 +0000

    :sparkles: feat(custom): now deploying to aws (#4)

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,26 @@
-import adapter from 'svelte-kit-sst'
-import preprocess from 'svelte-preprocess'
+import adapter from '@jill64/sveltekit-adapter-aws'
+import { sveltePreprocess } from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({
+	preprocess: sveltePreprocess({
 		postcss: true,
 	}),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			name: "hufghani-personal-website",
+			deploy: true,
+			architecture: 'edge-unbundled',
+			runtime: 'NODE_18',
+			cdn: true,
+			skipBootstrap: true,
+			domain:{
+				fqdn: "hufghani.dev",
+				certificateArn: "arn:aws:acm:us-east-1:078423179649:certificate/47bc68da-40cf-461a-afbc-ba12d8464087"
+			}
+		}),
 	},
 }
 
