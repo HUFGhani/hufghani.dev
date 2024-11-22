d5838d843ca8bb103b2437c160186863d9363715
Hamza Ghani <h.u.f.ghani@gmail.com>
2024-11-22 13:53:23 +0000

    :bug: fix(custom): fix deploy step

--- svelte.config.js
+++ svelte.config.js
@@ -3,19 +3,16 @@ import { sveltePreprocess } from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
+
 	preprocess: sveltePreprocess({
 		postcss: true,
 	}),
 	kit: {
 		adapter: adapter({
 			name: "hufghani-personal-website",
-			deploy: true,
 			architecture: 'edge-unbundled',
 			runtime: 'NODE_18',
 			cdn: true,
-			skipBootstrap: true,
 			domain:{
 				fqdn: "hufghani.dev",
 				certificateArn: "arn:aws:acm:us-east-1:078423179649:certificate/47bc68da-40cf-461a-afbc-ba12d8464087"
